5a237fc8f8bb5bd99cdfee6bc694981951d7f455
lmbarrera19 <lmbarrera@unbosque.edu.co>
2024-03-19 02:13:42 -0500

    Cambio registro de proyecto

--- src/main/resources/static/front-p2sw2/src/pages/Project/ProjectRegistration.tsx
+++ src/main/resources/static/front-p2sw2/src/pages/Project/ProjectRegistration.tsx
@@ -3,17 +3,16 @@ import DefaultLayout from '../../layout/DefaultLayout';
 import DatePickerOne from '../../components/Forms/DatePicker/DatePickerOne';
 import SelectGroupOne from '../../components/Forms/SelectGroup/SelectGroupOne';
 
-const RegistroProyecto = () => {
+const ProjectRegistration = () => {
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Registre un nuevo proyecto" />
+      <Breadcrumb pageName="Registre un nuevo Empleado" />
 
-      <div className="grid grid-cols-1  gap-9">
-        <div className="flex flex-col justify-center items-center gap-9">
-      
-          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="container mx-auto">
+        <div className=" justify-center items-center gap-9">
+          <div className="rounded-md border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-              <h3 className="font-medium text-black dark:text-white">
+              <h3 className="font-medium text-black dark:text-white text-center">
                 Proyecto
               </h3>
             </div>
@@ -41,8 +40,8 @@ const RegistroProyecto = () => {
               </div>
 
               <div className="mb-3 block text-black dark:text-white">
-                <DatePickerOne label="Fecha de inicio"/>
-                <DatePickerOne label="Fecha de finalización"/>
+                <DatePickerOne label="Fecha de inicio" />
+                <DatePickerOne label="Fecha de finalización" />
               </div>
 
               <div>
@@ -51,7 +50,7 @@ const RegistroProyecto = () => {
                 </label>
                 <input
                   type="number"
-                  placeholder="25000000.00"
+                  placeholder="2500.000.000"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
@@ -68,21 +67,18 @@ const RegistroProyecto = () => {
               </div>
 
               <div className="mb-3 block text-black dark:text-white">
-                  <SelectGroupOne />
+                <SelectGroupOne />
               </div>
 
-              <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
-                  Registrar 
-                </button>
-              
+              <button className="inline-flex items-center mx-auto justify-center rounded-full  bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+                Registrar
+              </button>
             </div>
           </div>
-          
         </div>
-
       </div>
     </DefaultLayout>
   );
 };
 
-export default RegistroProyecto;
+export default ProjectRegistration;
