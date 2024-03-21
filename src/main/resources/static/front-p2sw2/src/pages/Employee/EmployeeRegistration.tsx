a4e815573d715e68ecf6a85a88d63339ece3c348
lmbarrera19 <lmbarrera@unbosque.edu.co>
2024-03-20 23:06:01 -0500

    Cambio en el tipo documento y botón registrar

--- src/main/resources/static/front-p2sw2/src/pages/Employee/EmployeeRegistration.tsx
+++ src/main/resources/static/front-p2sw2/src/pages/Employee/EmployeeRegistration.tsx
@@ -1,7 +1,6 @@
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '../../layout/DefaultLayout';
 import DatePickerOne from '../../components/Forms/DatePicker/DatePickerOne';
-import SelectGroupOne from '../../components/Forms/SelectGroup/SelectGroupOne';
 
 const RegistroEmpleados = () => {
   return (
@@ -10,65 +9,102 @@ const RegistroEmpleados = () => {
 
       <div className="container mx-auto">
         <div className=" justify-center items-center gap-9">
-          <div className="rounded-md border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      
+          <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
-              <h3 className="font-medium text-black dark:text-white text-center">
-                Proyecto
+              <h3 className="font-medium text-black dark:text-white">
+                Empleado
               </h3>
             </div>
-            <div className="flex flex-col gap-5.5 p-6.5">
+            <div className="flex flex-col gap-5.5 p-7.5">
               <div>
                 <label className="mb-3 block text-black dark:text-white">
                   Nombre
                 </label>
                 <input
                   type="text"
-                  placeholder="Nombre del proyecto"
+                  placeholder="Nombre del empleado"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
 
               <div>
                 <label className="mb-3 block text-black dark:text-white">
-                  Descripción
+                  Dirección
                 </label>
                 <input
                   type="text"
-                  placeholder="El proyecto..."
+                  placeholder="Dirección"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
-              </div>
-
-              <div className="mb-3 block text-black dark:text-white">
-                <DatePickerOne label="Fecha de inicio" />
-                <DatePickerOne label="Fecha de finalización" />
               </div>
 
               <div>
                 <label className="mb-3 block text-black dark:text-white">
-                  Presupuesto
+                  Salario
                 </label>
                 <input
                   type="number"
-                  placeholder="2500.000.000"
+                  placeholder="5000000.00"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                />
+              </div>
+
+              <div className="mb-3 block text-black dark:text-white">
+                <DatePickerOne label="Fecha de ingreso"/>
+                <DatePickerOne label="Fecha de nacimiento"/>
+              </div>
+
+              <div className="mb4">
+                  <label className="mb-2.5 block font-medium text-black dark:text-white">
+                    Tipo De Documento
+                  </label>
+                  <div className="relative">
+                    <select
+                      name=""
+                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                    >
+                      <option selected disabled>
+                        Seleccione una opción
+                      </option>
+                      <option value="1">Cédula Ciudadanía (C.C)</option>
+                      <option value="2">Cédula Extranjería (C.E)</option>
+                      <option value="3">Pasaporte (P.A)</option>
+                    </select>
+                  </div>
+                </div>
+
+              <div>
+                <label className="mb-3 block text-black dark:text-white">
+                  Número de documento
+                </label>
+                <input
+                  type="text"
+                  placeholder="Número de documento"
                   className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
 
               <div>
-              <SelectGroupOne labelText='Estado' selectPlaceholder={'En progreso'}/>
-              </div>
-
-              <div className="mb-3 block text-black dark:text-white">
-                <SelectGroupOne labelText='Tipo de proyecto' selectPlaceholder={'Tipo de proyecto'}/>
+                <label className="mb-3 block text-black dark:text-white">
+                  Teléfono
+                </label>
+                <input
+                  type="text"
+                  placeholder="Teléfono"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                />
               </div>
 
               <button className="inline-flex items-center mx-auto justify-center rounded-full  bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
-                Registrar
-              </button>
+                  Registrar 
+                </button>
+              
             </div>
           </div>
+          
         </div>
+
       </div>
     </DefaultLayout>
   );
