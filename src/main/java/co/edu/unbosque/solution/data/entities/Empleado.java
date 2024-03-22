9e61b6a90bb9ff3de9ecaa1a027ec2f948240904
lmbarrera19 <lmbarrera@unbosque.edu.co>
2024-03-22 00:53:29 -0500

    Update Empleado.java

--- src/main/java/co/edu/unbosque/solution/data/entities/Empleado.java
+++ src/main/java/co/edu/unbosque/solution/data/entities/Empleado.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
+
+import org.hibernate.proxy.HibernateProxy;
 
 /**
  * The type Empleado.
@@ -24,13 +28,13 @@ public class Empleado implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEmpleado;
 
-    @Column(name = "nombre")
+    @Column(name = "nombre", length = 100)
     private String nombre;
 
     @Column(name = "direccion")
     private String direccion;
 
-    @Column(name = "salario")
+    @Column(name = "salario", precision = 10, scale = 2)
     private BigDecimal salario;
 
     @Column(name = "fecha_ingreso")
@@ -46,13 +50,19 @@ public class Empleado implements Serializable{
     @Column(name = "numero_documento", length = 50)
     private String numero_documento;
 
-    @Column(name = "telefono")
+    @Column(name = "telefono", length = 20)
     private String telefono;
 
-    @Column(name = "nombre_completo", nullable = false)
-    private String nombreCompleto;
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Empleado empleado = (Empleado) o;
+        return getNumero_documento() != null && Objects.equals(getNumero_documento(), empleado.getNumero_documento());
+    }
 
-    @Column(name = "id_tipo_documento", nullable = false)
-    private Integer idTipoDocumento;
 
 }
