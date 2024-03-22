2453171c54a70b35586ab51433591e46d66c2631
moneitor26 <dfrghp@gmail.com>
2024-03-22 00:50:31 -0500

    Base de datos actualizada sin errores

--- src/main/resources/db/migration/V1__CREATE_DB.sql
+++ src/main/resources/db/migration/V1__CREATE_DB.sql
@@ -1,17 +1,16 @@
-
 -- tabla tipo documento --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.tipodocumento (
-  `idTipoDocumento` int NOT NULL AUTO_INCREMENT,
-  `tipoDocumento` varchar(100) NOT NULL unique,
-  PRIMARY KEY (`idTipoDocumento`)
+                                                         `idTipoDocumento` int NOT NULL AUTO_INCREMENT,
+                                                         `tipoDocumento` varchar(100) NOT NULL unique,
+                                                         PRIMARY KEY (`idTipoDocumento`)
 );
 
 -- tabla rol --
 CREATE TABLE IF NOT EXISTS proyectoabc.rol (
-  `idRol` int NOT NULL AUTO_INCREMENT,
-  `rol` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idRol`)
+                                               `idRol` int NOT NULL AUTO_INCREMENT,
+                                               `rol` varchar(100) DEFAULT NULL,
+                                               PRIMARY KEY (`idRol`)
 );
 
 
@@ -28,20 +27,6 @@ CREATE TABLE IF NOT EXISTS proyectoabc.usuario (
 
 -- tabla empleado --
 CREATE TABLE IF NOT EXISTS proyectoabc.empleado (
-<<<<<<< Updated upstream
-  `idEmpleado` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `salario` decimal(10,2) DEFAULT NULL,
-  `fecha_ingreso` date DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `tipo_documento_id` int DEFAULT NULL,
-  `numero_documento` varchar(50) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`idEmpleado`),
-  KEY `tipo_documento_id` (`tipo_documento_id`),
-  CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`tipo_documento_id`) REFERENCES `proyectoabc`.`tipodocumento` (`idTipoDocumento`)
-=======
                                                     `idEmpleado` INT NOT NULL AUTO_INCREMENT,
                                                     `nombre` VARCHAR(100) DEFAULT NULL,
                                                     `direccion` VARCHAR(255) DEFAULT NULL,
@@ -55,65 +40,64 @@ CREATE TABLE IF NOT EXISTS proyectoabc.empleado (
                                                     PRIMARY KEY (`idEmpleado`),
                                                     KEY `tipo_documento_id` (`tipo_documento_id`),
                                                     CONSTRAINT `fk_usuario_empleado` FOREIGN KEY (`id_usuario`) REFERENCES `proyectoabc`.`usuario` (`id_usuario`)
->>>>>>> Stashed changes
 );
 
 
 -- tabla empleado_rol --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.empleado_rol (
-  `idEmpleadoRol` int NOT NULL AUTO_INCREMENT,
-  `idEmpleado` int NOT NULL,
-  `idRol` int NOT NULL,
-  PRIMARY KEY (`idEmpleadoRol`),
-  KEY `idEmpleado` (`idEmpleado`),
-  KEY `idRol` (`idRol`),
-CONSTRAINT `empleado_rol_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`),
-CONSTRAINT `empleado_rol_ibfk_2` FOREIGN KEY (`idRol`) REFERENCES `proyectoabc`.`rol` (`idRol`)
+                                                        `idEmpleadoRol` int NOT NULL AUTO_INCREMENT,
+                                                        `idEmpleado` int NOT NULL,
+                                                        `idRol` int NOT NULL,
+                                                        PRIMARY KEY (`idEmpleadoRol`),
+                                                        KEY `idEmpleado` (`idEmpleado`),
+                                                        KEY `idRol` (`idRol`),
+                                                        CONSTRAINT `empleado_rol_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`),
+                                                        CONSTRAINT `empleado_rol_ibfk_2` FOREIGN KEY (`idRol`) REFERENCES `proyectoabc`.`rol` (`idRol`)
 );
 
 -- tabla equipo proyecto --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.equipoproyecto (
-  `idEquipoProyecto` int NOT NULL AUTO_INCREMENT,
-  `nombre_equipo` varchar(100) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `fecha_creacion` date DEFAULT NULL,
-  `idEmpleado` int DEFAULT NULL,
-  PRIMARY KEY (`idEquipoProyecto`),
-  KEY `idEmpleado` (`idEmpleado`),
-  CONSTRAINT `equipoproyecto_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
+                                                          `idEquipoProyecto` int NOT NULL AUTO_INCREMENT,
+                                                          `nombre_equipo` varchar(100) DEFAULT NULL,
+                                                          `descripcion` varchar(255) DEFAULT NULL,
+                                                          `fecha_creacion` date DEFAULT NULL,
+                                                          `idEmpleado` int DEFAULT NULL,
+                                                          PRIMARY KEY (`idEquipoProyecto`),
+                                                          KEY `idEmpleado` (`idEmpleado`),
+                                                          CONSTRAINT `equipoproyecto_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
 );
 
 -- tabla proyecto --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.proyecto (
-  `idProyecto` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_fin_prevista` date DEFAULT NULL,
-  `presupuesto` decimal(10,2) DEFAULT NULL,
-  `estado` varchar(50) DEFAULT NULL,
-  `idLiderProyecto` int DEFAULT NULL,
-  PRIMARY KEY (`idProyecto`),
-  KEY `idLiderProyecto` (`idLiderProyecto`),
-  CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`idLiderProyecto`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
+                                                    `idProyecto` int NOT NULL AUTO_INCREMENT,
+                                                    `nombre` varchar(255) DEFAULT NULL,
+                                                    `descripcion` varchar(255) DEFAULT NULL,
+                                                    `fecha_inicio` date DEFAULT NULL,
+                                                    `fecha_fin_prevista` date DEFAULT NULL,
+                                                    `presupuesto` decimal(10,2) DEFAULT NULL,
+                                                    `estado` varchar(50) DEFAULT NULL,
+                                                    `idLiderProyecto` int DEFAULT NULL,
+                                                    PRIMARY KEY (`idProyecto`),
+                                                    KEY `idLiderProyecto` (`idLiderProyecto`),
+                                                    CONSTRAINT `proyecto_ibfk_1` FOREIGN KEY (`idLiderProyecto`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
 );
 
 -- tabla historial cambios --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.historialcambios (
-  `idHistorial` int NOT NULL AUTO_INCREMENT,
-  `idProyecto` int DEFAULT NULL,
-  `fecha_cambio` date DEFAULT NULL,
-  `descripcion_cambio` text,
-  `idEmpleado` int DEFAULT NULL,
-  PRIMARY KEY (`idHistorial`),
-  KEY `idProyecto` (`idProyecto`),
-  KEY `idEmpleado` (`idEmpleado`),
-CONSTRAINT `historialcambios_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
-CONSTRAINT `historialcambios_ibfk_2` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
+                                                            `idHistorial` int NOT NULL AUTO_INCREMENT,
+                                                            `idProyecto` int DEFAULT NULL,
+                                                            `fecha_cambio` date DEFAULT NULL,
+                                                            `descripcion_cambio` text,
+                                                            `idEmpleado` int DEFAULT NULL,
+                                                            PRIMARY KEY (`idHistorial`),
+                                                            KEY `idProyecto` (`idProyecto`),
+                                                            KEY `idEmpleado` (`idEmpleado`),
+                                                            CONSTRAINT `historialcambios_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
+                                                            CONSTRAINT `historialcambios_ibfk_2` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
 );
 
 
@@ -121,12 +105,12 @@ CONSTRAINT `historialcambios_ibfk_2` FOREIGN KEY (`idEmpleado`) REFERENCES `proy
 -- tabla proyectoequipo --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.proyectoequipo (
-  `idProyecto` int NOT NULL ,
-  `idEquipoProyecto` int NOT NULL,
-  PRIMARY KEY (`idProyecto`,`idEquipoProyecto`),
-  KEY `idEquipoProyecto` (`idEquipoProyecto`),
-  CONSTRAINT `proyectoequipo_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
-  CONSTRAINT `proyectoequipo_ibfk_2` FOREIGN KEY (`idEquipoProyecto`) REFERENCES `proyectoabc`.`equipoproyecto` (`idEquipoProyecto`)
+                                                          `idProyecto` int NOT NULL ,
+                                                          `idEquipoProyecto` int NOT NULL,
+                                                          PRIMARY KEY (`idProyecto`,`idEquipoProyecto`),
+                                                          KEY `idEquipoProyecto` (`idEquipoProyecto`),
+                                                          CONSTRAINT `proyectoequipo_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
+                                                          CONSTRAINT `proyectoequipo_ibfk_2` FOREIGN KEY (`idEquipoProyecto`) REFERENCES `proyectoabc`.`equipoproyecto` (`idEquipoProyecto`)
 );
 
 
@@ -134,61 +118,33 @@ CREATE TABLE IF NOT EXISTS proyectoabc.proyectoequipo (
 -- tabla seguimiento proyecto  --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.seguimientoproyecto (
-  `idSeguimiento` int NOT NULL AUTO_INCREMENT,
-  `idProyecto` int DEFAULT NULL,
-  `fecha_seguimiento` date DEFAULT NULL,
-  `observaciones` text,
-  PRIMARY KEY (`idSeguimiento`),
-  KEY `idProyecto` (`idProyecto`),
-  CONSTRAINT `seguimientoproyecto_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`)
+                                                               `idSeguimiento` int NOT NULL AUTO_INCREMENT,
+                                                               `idProyecto` int DEFAULT NULL,
+                                                               `fecha_seguimiento` date DEFAULT NULL,
+                                                               `observaciones` text,
+                                                               PRIMARY KEY (`idSeguimiento`),
+                                                               KEY `idProyecto` (`idProyecto`),
+                                                               CONSTRAINT `seguimientoproyecto_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`)
 );
 
 -- tabla seguimiento tiempo --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.seguimientotiempo (
-  `idSeguimientoTiempo` int NOT NULL AUTO_INCREMENT,
-  `idProyecto` int DEFAULT NULL,
-  `idEmpleado` int DEFAULT NULL,
-  `horas_trabajadas` decimal(10,2) DEFAULT NULL,
-  `fecha_registro` date DEFAULT NULL,
-  PRIMARY KEY (`idSeguimientoTiempo`),
-  KEY `idProyecto` (`idProyecto`),
-  KEY `idEmpleado` (`idEmpleado`),
- CONSTRAINT `seguimientotiempo_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
- CONSTRAINT `seguimientotiempo_ibfk_2` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
+                                                             `idSeguimientoTiempo` int NOT NULL AUTO_INCREMENT,
+                                                             `idProyecto` int DEFAULT NULL,
+                                                             `idEmpleado` int DEFAULT NULL,
+                                                             `horas_trabajadas` decimal(10,2) DEFAULT NULL,
+                                                             `fecha_registro` date DEFAULT NULL,
+                                                             PRIMARY KEY (`idSeguimientoTiempo`),
+                                                             KEY `idProyecto` (`idProyecto`),
+                                                             KEY `idEmpleado` (`idEmpleado`),
+                                                             CONSTRAINT `seguimientotiempo_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`),
+                                                             CONSTRAINT `seguimientotiempo_ibfk_2` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
 );
 
 -- tabla tarea --
 
 CREATE TABLE IF NOT EXISTS proyectoabc.tarea (
-<<<<<<< Updated upstream
-  `idTarea` int NOT NULL AUTO_INCREMENT,
-  `idProyecto` int DEFAULT NULL,
-  `descripcion` text,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_fin_prevista` date DEFAULT NULL,
-  `estado` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idTarea`),
-  KEY `idProyecto` (`idProyecto`),
-  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`)
-);
-
-
-
-
--- tabla usuario  --
-CREATE TABLE IF NOT EXISTS proyectoabc.usuario (
-  `id_usuario` int NOT NULL AUTO_INCREMENT,
-  `nombre_usuario` varchar(100) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `idEmpleado` int DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE KEY `email` (`email`),
-  KEY `idEmpleado` (`idEmpleado`),
-  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `proyectoabc`.`empleado` (`idEmpleado`)
-) ;
-=======
                                                  `idTarea` int NOT NULL AUTO_INCREMENT,
                                                  `idProyecto` int DEFAULT NULL,
                                                  `descripcion` text,
@@ -199,4 +155,3 @@ CREATE TABLE IF NOT EXISTS proyectoabc.usuario (
                                                  KEY `idProyecto` (`idProyecto`),
                                                  CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`idProyecto`) REFERENCES `proyectoabc`.`proyecto` (`idProyecto`)
 );
->>>>>>> Stashed changes
